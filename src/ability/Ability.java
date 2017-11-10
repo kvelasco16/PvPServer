6ab0e94ed8209008f663f31f454a488b9831365c
Erick <rerick256@gmail.com>
2017-11-10 00:40:25 -0800

    Add abstract method for sound

--- src/ability/Ability.java
+++ src/ability/Ability.java
@@ -1,7 +1,10 @@
 package ability;
 
+import org.bukkit.Sound;
+
 public interface Ability
 {
 	// Method for performing the ability itself
 	abstract void use();
+	abstract void playSound(Sound sound);
 }
