fb969b6252c6a7bb13f1a868ba0acee8f4ffbd90
Erick <rerick256@gmail.com>
2017-11-15 01:52:23 -0800

    Removed unnecessary methods and variables (if any). Methods to access and modify attributes are now in the Physical abstract class

---
+++ src/ability/Fireblast.java
@@ -0,0 +1,22 @@
+package ability;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class Fireblast extends Physical
+{
+	// TODO: Don't create an object of this class for usage. Call the use method for this class only. See if it works.
+	@Override
+	public void use(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Entity getEntity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
--- src/ability/Physical.java
+++ src/ability/Physical.java
@@ -57,7 +57,7 @@ public abstract class Physical implements Ability
 	public int getAbilityPower()
 	{
 		return abilityPower;
-	}	
+	}
 }
 
 
