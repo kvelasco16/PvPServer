fd28465400f11752ce388cbc134f5100d8308362
Erick <rerick256@gmail.com>
2017-11-20 21:01:01 -0800

    Add enum AbilityList.java

---
+++ src/ability/AbilityList.java
@@ -0,0 +1,24 @@
+package ability;
+
+public enum AbilityList 
+{
+// List names in alphabetical order!!
+	BATTLE_SHOUT,
+	BEG,
+	BLINK,
+	COLOSSAL_SMASH,
+	DIVINE_SHIELD,
+	EXPLOSIVE_SHOT,
+	FIREBALL,
+	GIVE_IN,
+	HAND_OF_PROTECTION,
+	HEAL,
+	ICE_SHARD,
+	LEAP,
+	MUG,
+	POISON,
+	RAPID_FIRE,
+	SPRINT,
+	STEADY_SHOT,
+	STEALTH,
+}
